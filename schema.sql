b108eee54d84348ad8136c7d1e926337ba4dfc57
Charlene Glasgow <And2reaC@aol.com>
2020-07-03 18:26:20 -0400

    corrected code, titles does not have unique emp no, primary key removed

--- schema.sql
+++ schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE employees (
     PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
+CREATE TABLE dept_managers (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
@@ -40,16 +40,16 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-CREATE TABLE dept_managers (
-	dept_no VARCHAR NOT NULL,
+CREATE TABLE dept_employee (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
+

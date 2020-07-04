4567965015cd4a373d2aee95ef2fa6bcaeef86c8
Charlene Glasgow <And2reaC@aol.com>
2020-07-03 21:34:45 -0400

    SQL queries

---
+++ queries.sql
@@ -0,0 +1,29 @@
+SELECT * FROM departments;
+SELECT * FROM dept_managers;
+SELECT * FROM dept_employee;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- create data table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+--  show data on new table
+SELECT * FROM retirement_info;
